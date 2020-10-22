3b1b90e8bd0a21993e6ee5edd1be37ec0ab4b08a
Joshuah Martin <joshuahmartin@Joshuahs-MacBook-Pro.local>
2020-10-22 16:51:37 -0400

    build user resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'jxf', password: 'banana' )
+User.create(username: 'jpf', password: 'banana' )
+User.create(username: 'jll', password: 'banana' )
+User.create(username: 'jgg', password: 'banana' )
+User.create(username: 'jyy', password: 'banana' )
